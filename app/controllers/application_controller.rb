7db95a186bb7621c170f16f6929ca241a9e28a1c
danielRomero <daniel.romero.esteban@gmail.com>
2015-03-11 17:08:25 +0100

    locale fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,23 +12,24 @@ class ApplicationController < ActionController::Base
   end
 
   def change_locale
+    #raise request.inspect
     redir = []
     request.referer.split('/').each do |a|
       a = I18n.locale if LOCALES.include?(a)
       redir << a
     end
-    redirect_to redir.join('/')
+    redirect_to "#{redir.join('/')}?locale=#{I18n.locale}"
   end
 
   private
     def set_locale
       if params[:locale].blank?
+        # locale from browser
         logger.debug "[LOCALE] Accept-Language: #{request.env['HTTP_ACCEPT_LANGUAGE']} - Params: #{params[:locale]}"
         I18n.locale = request.env['HTTP_ACCEPT_LANGUAGE'].blank? ? I18n.default_locale : request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
       elsif LOCALES.include?(params[:locale])
+        # locale from params
         I18n.locale = params[:locale] || I18n.default_locale
-      else
-        I18n.locale =  I18n.default_locale
       end
       logger.debug "[LOCALE] Locale set to '#{I18n.locale}'"
     end
