69370a03dc410f6183befb3e9a2465cab74e492b
Daniel Romero <daniel.romero.esteban@gmail.com>
2015-10-01 19:09:28 +0200

    ++

--- config/application.rb
+++ config/application.rb
@@ -30,14 +30,17 @@ module Danielromero
     config.action_mailer.perform_deliveries = true
 
     config.action_mailer.delivery_method = :smtp
+
+    config.action_mailer.default_url_options = { host:ENV['HOST'] }
+    config.action_mailer.default :charset => "utf-8"
     config.action_mailer.smtp_settings = {
-      :address => ENV['SMTP_ADDRESS'],
-      :port => ENV['SMTP_PORT'],
-      :domain => ENV['SMTP_DOMAIN'],
-      :user_name => ENV['DEV_EMAIL_ADDRESS'],
-      :password => ENV['DEV_EMAIL_PASS'],
-      :authentication => ENV['SMTP_AUTHENTICATION'],
-      :enable_starttls_auto => true
+        :address => ENV['SMTP_ADDRESS'],
+        :port => ENV['SMTP_PORT'],
+        :domain => ENV['HOST'],
+        :user_name => ENV['DEV_EMAIL_ADDRESS'],
+        :password => ENV['DEV_EMAIL_PASS'],
+        :authentication => :plain,
+        :enable_starttls_auto => true
     }
   end
 end
