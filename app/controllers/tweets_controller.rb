ac6c2b8e98382996863c4e58e82c3ded7d5ef923
Daniel Romero <daniel.romero.esteban@gmail.com>
2015-10-05 13:30:06 +0200

    bugfix

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,5 +2,9 @@ class TweetsController < ApplicationController
 
   def index
     @page = (params[:page].blank? or params[:page].to_i < 0) ? 0 : params[:page].to_i
+    respond_to do |format|
+      format.js { render }
+      format.html { redirect_to root_locale_path(I18n.locale) }
+    end
   end
 end
